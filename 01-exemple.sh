af296fcc00dc8ed8477aff4a01711274ef117d14
isx48067911 <isx48067911@j29.informatica.escoladeltreball.org>
2021-02-08 13:30:38 +0100

    01-exemple.sh

---
+++ 01-exemple.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# @isx48067911 ASIX-M01
+# Febrero 2021
+# Primer ejemplo scrip
+#-----------------------------------
+echo "Hello World"
+echo "bon dia, avui plou"
+nom="pere puig pout"
+edat=18
+echo $nom $edat
+echo -e "nom: $nom\n edat: $edat"
+echo -e 'nom: $nom\t edat: $edat'
+uname -a
+uptime
+echo $SHELL
+echo $SHLVL
+echo $((4*32))
+echo $((edat*2))
+#read data data2
+echo -e "$data \n $data2"
+exit 0
