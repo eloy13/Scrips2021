b58b11e8b4a11d4420cd75f0ec14903a3d889a34
eloy <eloy@localhost.localdomain>
2021-02-17 13:05:13 +0100

    “scrip01”

---
+++ 11-exemplesfor.sh
@@ -0,0 +1,23 @@
+# !/ in/bash
+# @edt ASIX M01-ISO
+# Febrer 2021
+# Decir el numero de dias del mes
+# -------------------------------------
+
+#for var in llista de valors 
+#do
+#     acions
+#done
+#1)itenera una llista de noms
+for nom in "pere" "marta" "anna" "ramon"
+do
+  echo $nom
+done
+exit 0
+
+for nom in "pere" "marta" "anna" "ramon"
+do
+  echo $nom
+done
+exit 0
+
